fe47daecfae4d165a7aabbcaf3138bbb0f9d5e81
nikita <chepik29rus@gmail.com>
2019-07-20 17:20:42 +0300

    Project files.

---
+++ webapp/Models/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace webapp.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        [Required]
+        public string ShortName { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        [Required]
+        public int ShelfNumber { get; set; }
+        [Required]
+        public double Weight { get; set; }
+        [Required(ErrorMessage = "Date in is required")]
+        public DateTime DateIn { get; set; }
+        [Required(ErrorMessage = "Date out is required")]
+        public DateTime DateOut { get; set; }
+
+    }
+}
